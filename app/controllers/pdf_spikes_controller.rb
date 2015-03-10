9ead5bbf67adeb3740449d468951d328167aaaa2
kevin <faithful.7up@gmail.com>
2015-03-10 12:56:38 +0700

    add layout

--- app/controllers/pdf_spikes_controller.rb
+++ app/controllers/pdf_spikes_controller.rb
@@ -1,9 +1,12 @@
 class PdfSpikesController < ApplicationController
+  before_action :set_pdfSpike, only: [:show, :edit, :update, :destroy]
   def index
     @pdfs = PdfSpike.all
   end
   def new
     @pdf= PdfSpike.new
+  end
+  def edit
   end
   def create
     @pdf= PdfSpike.new(pdf_spike_params)
@@ -14,10 +17,23 @@ class PdfSpikesController < ApplicationController
     end
   end
   def show
-    @pdf = PdfSpike.find(params[:id])
+  end
+  def update
+    if @pdf.update(pdf_spike_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
+  def destroy
+    @pdf.destroy
+    redirect_to pdf_spikes_path
+  end
   private
+  def set_pdfSpike
+    @pdf = PdfSpike.find(params[:id])
+  end
   def pdf_spike_params
     params.require(:pdf_spike).permit(:title,:detail,:text)
   end
